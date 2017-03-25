40ae965cbff2820de8746f21478ebcdae014a84a
Ryan Anderson <ryan@rycander.com>
2017-03-25 13:43:20 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const schedule = require('node-schedule');
+const client = new Discord.Client();
+
+client.login('MjkxNDUwODgwMjc0NzI2OTIz.C7hkMA.hOBTTaqEBkQmlZfW_egKwXwBhH4');
+
+client.on('ready', () => {
+
+});
+
+client.on('message', (message) => {
+  let channel = message.channel;
+  if(!message.author.bot) {
+    channel.sendMessage('Cool story bro.'); 
+  }
+});
+
+prep() {
+  let channel = client.channels.find('name', 'general');
+
+}
+
